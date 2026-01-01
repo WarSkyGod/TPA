1f2688f1d823072b43a4bba4c9c61649d95a7356
WarSkyGod <wargod@craft-hello.top>
2026-01-01 23:23:24 +0800

    4.0-Refactoring

--- src/main/java/top/craft_hello/tpa/enums/TimerType.java
+++
@@ -1,5 +0,0 @@
-package top.craft_hello.tpa.enums;
-
-public enum TimerType {
-    TELEPORT, WARP_TELEPORT, HOME_TELEPORT, SPAWN_TELEPORT, BACK_TELEPORT, DENY
-}
--- src/main/java/top/craft_hello/tpa/interfaces/ConfigurationInterface.java
+++
@@ -1,5 +0,0 @@
-package top.craft_hello.tpa.interfaces;
-
-public interface ConfigurationInterface {
-    void reloadConfiguration();
-}
